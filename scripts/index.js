2fdd2d3d19e3c938d5c8773581afe0e36cf03772
BackbenchCoder-py <80917802+BackbenchCoder-py@users.noreply.github.com>
2022-05-02 12:04:25 +0530

    news-app

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+// Ude Import export (MANDATORY)
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+
+import {navbar, sidebar} from "../components/navSidebar.js";
+document.querySelector('header').innerHTML = navbar();
+document.querySelector('#sidebar').innerHTML = sidebar();
+
+let search = (e) => {
+    if(e.key == 'Enter') {
+        let query = document.querySelector('#search_input').value;
+        searchNews(query).then((data) => {
+            console.log(data);
+            localStorage.setItem('data', JSON.stringify(data));
+            window.location.href = 'search.html';
+            // append(data, document.querySelector('#results'));
+        })
+    }
+}
+document.querySelector('#search_input').addEventListener('keydown', search);
+
+import { searchNews, append, searchNewsCountry } from "./fetch.js";
+
+let countries = document.querySelector("#sidebar").children;
+
+function cSearch() {
+    window.location.href = 'news.html';
+    console.log(this.id);
+    searchNewsCountry(this.id).then((data) => {
+        console.log(data);
+        append(data, document.querySelector('#detailed_news'));
+    })
+}
+
+for(let el of countries) {
+    el.addEventListener('click', cSearch);
+}
+
+console.log(countries);
